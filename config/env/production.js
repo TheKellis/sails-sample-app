80db8c14c236d5be48582737c38a7989bab0c1a0
Kevin Ellis <kevinellisdev@gmail.com>
2020-10-21 18:45:52 -0700

    Updated config for dns

--- config/env/production.js
+++ config/env/production.js
@@ -340,7 +340,7 @@ module.exports = {
   * this, just try deploying without setting it and see if it works.)       *
   *                                                                         *
   ***************************************************************************/
-  // port: 80,
+  port: 80,
 
 
 
@@ -374,7 +374,7 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://54.219.206.2',
+    baseUrl: 'http://flagrantfox.com',
     internalEmailAddress: 'support@example.com',
 
     // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
