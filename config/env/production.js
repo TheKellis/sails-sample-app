181d1eb8bf3d7976d4dc7ef8fb47e370e0b8ee03
Kevin Ellis <kevinellisdev@gmail.com>
2020-10-28 17:03:30 -0700

    Update port and baseUrl

--- config/env/production.js
+++ config/env/production.js
@@ -375,7 +375,7 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'flagrantfox.com',
+    baseUrl: 'http://flagrantfox.com',
     internalEmailAddress: 'support@example.com',
 
     // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
